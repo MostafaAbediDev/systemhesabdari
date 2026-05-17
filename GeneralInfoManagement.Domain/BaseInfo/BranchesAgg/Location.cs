c4fc5d78a44e24a4b6e22e1eed33efc3e875e262
Mostafa Abedi <mostafaabedi.developer@gamil.com>
2026-05-17 10:57:59 +0330

    ChangeSomeProperyDataType And Add Location Insted Of Lat_log In GeneralInfo

---
+++ GeneralInfoManagement.Domain/BaseInfo/BranchesAgg/Location.cs
@@ -0,0 +1,17 @@
+﻿namespace GeneralInfoManagement.Domain.BaseInfo.BranchesAgg
+{
+    public class Location
+    {
+        public double Latitude { get; private set; }
+        public double Longitude { get; private set; }
+
+        protected Location() { }
+
+        public Location(double latitude, double longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+    }
+
+}
---
+++ GeneralInfoManagement.Infrastructure.EFCore/Migrations/20260517065939_ChangeSomePropertyType.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeneralInfoManagement.Infrastructure.EFCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeSomePropertyType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RegisterNumber",
+                table: "Branches",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NationalId",
+                table: "Branches",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EconomicCode",
+                table: "Branches",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "RegisterNumber",
+                table: "Branches",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "NationalId",
+                table: "Branches",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EconomicCode",
+                table: "Branches",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
