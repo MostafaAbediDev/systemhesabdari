d2a15211c225cd531ce6e356a5189f84d0ddff32
Mostafa Abedi <mostafaabedi.developer@gamil.com>
2026-05-23 12:20:58 +0330

    Company Repository Implemented

--- GeneralInfoManagement.Infrastructure.EFCore/Repository/CompanyRepository.cs
+++ GeneralInfoManagement.Infrastructure.EFCore/Repository/CompanyRepository.cs
@@ -15,17 +15,51 @@ namespace GeneralInfoManagement.Infrastructure.EFCore.Repository
 
         public List<CompanyViewModel> GetCompanies()
         {
-            throw new NotImplementedException();
+            return _context.Companies
+                .Select(x => new CompanyViewModel
+                {
+                    Id = x.Id,
+                    Title = x.Title,
+                    LegalName = x.LegalName,
+                    CreationDate = x.CreationDate.ToString()
+                })
+                .OrderByDescending(x => x.Id)
+                .ToList();
         }
 
         public EditCompanies GetDetails(long id)
         {
-            throw new NotImplementedException();
+            return _context.Companies
+                .Select(x => new EditCompanies
+                {
+                    Id = x.Id,
+                    Title = x.Title,
+                    LegalName = x.LegalName,
+                    Logo = x.Logo,
+                    EstablishedDate = x.EstablishedDate
+                })
+                .FirstOrDefault(x => x.Id == id);
         }
 
         public List<CompanyViewModel> Search(CompanySearchModel searchModel)
         {
-            throw new NotImplementedException();
+            var query = _context.Companies
+                            .Select(x => new CompanyViewModel
+                            {
+                                Id = x.Id,
+                                Title = x.Title,
+                                LegalName = x.LegalName,
+                                CreationDate = x.CreationDate.ToString()
+                            });
+
+            // فیلترگذاری پویا
+            if (!string.IsNullOrWhiteSpace(searchModel.Title))
+                query = query.Where(x => x.Title.Contains(searchModel.Title));
+
+            if (!string.IsNullOrWhiteSpace(searchModel.LegalName))
+                query = query.Where(x => x.LegalName.Contains(searchModel.LegalName));
+
+            return query.OrderByDescending(x => x.Id).ToList();
         }
     }
 }
