5a23a50d3eab7eb4538834648c8e975742e5b1b5
Mostafa Abedi <mostafaabedi.developer@gamil.com>
2026-05-25 10:52:49 +0330

    Create FakeData For GeneralInfo Entities

---
+++ GeneralInfoManagement.Infrastructure.EFCore/Migrations/FakeData/20260525071032_CreateSeedFinancialPeriodFakeData.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeneralInfoManagement.Infrastructure.EFCore.Migrations.FakeData
+{
+    /// <inheritdoc />
+    public partial class CreateSeedFinancialPeriodFakeData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DECLARE @branchId INT = 1001;
+
+                WHILE @branchId <= 1500
+                BEGIN
+                    INSERT INTO FinancialPeriods
+                    (
+                        Title,
+                        StartDate,
+                        EndDate,
+                        BranchId,
+                        IsDeleted,
+                        IsActive,
+                        CreationDate
+                    )
+                    VALUES
+                    (
+                        CONCAT(N'دوره مالی شعبه ', @branchId),
+                        '2024-03-21',
+                        '2025-03-20',
+                        @branchId,
+                        0,
+                        1,
+                        GETDATE()
+                    );
+
+                    SET @branchId = @branchId + 1;
+                END;
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                DELETE FROM FinancialPeriods
+                WHERE Title LIKE N'دوره مالی شعبه %'
+                  AND BranchId BETWEEN 1001 AND 1500
+            ");
+        }
+    }
+}
---
+++ GeneralInfoManagement.Infrastructure.EFCore/Migrations/FakeData/20260525072034_UpdatePictureFakeData.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeneralInfoManagement.Infrastructure.EFCore.Migrations.FakeData
+{
+    /// <inheritdoc />
+    public partial class UpdatePictureFakeData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+
+                WITH CTE_Pictures AS (
+                    SELECT TOP (500)
+                           Id,
+                           ROW_NUMBER() OVER (ORDER BY Id) AS NewOwnerId
+                    FROM Pictures
+                    WHERE IsDeleted = 0
+                )
+                UPDATE p
+                SET 
+                    p.OwnerId = c.NewOwnerId,
+                    p.OwnerType = 1, -- Product
+                    p.Url = CONCAT('/assets/images/product_', c.NewOwnerId, '.jpg'),
+                    p.IsMain = CASE WHEN c.NewOwnerId = 1 THEN 1 ELSE 0 END,
+                    p.IsActive = 1
+                FROM Pictures p
+                INNER JOIN CTE_Pictures c ON p.Id = c.Id;
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                PRINT 'Rollback of Picture update data is not automatically supported without backup.';
+            ");
+        }
+    }
+}
