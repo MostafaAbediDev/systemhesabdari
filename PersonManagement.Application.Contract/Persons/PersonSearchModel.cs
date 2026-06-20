20adddb45a434c46f5d2a4c01dda839985d5c703
Mostafa Abedi <mostafaabedi.developer@gamil.com>
2026-06-20 12:23:06 +0330

    FullName Split to First And Last Name From Persons

--- PersonManagement.Application.Contract/Persons/PersonSearchModel.cs
+++ PersonManagement.Application.Contract/Persons/PersonSearchModel.cs
@@ -2,7 +2,8 @@
 {
     public class PersonSearchModel
     {
-        public string FullName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string NationalCode { get; set; }
         public string EconomicCode { get; set; }
         public string RegistrationNumber { get; set; }
