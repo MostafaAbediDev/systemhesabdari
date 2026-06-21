4e04fe57e8aa1f805e7ee9d6778b165feef16426
Mostafa Abedi <mostafaabedi.developer@gamil.com>
2026-06-21 12:52:17 +0330

    BankRepository And Application Implemented

---
+++ BankManagement.Application.Contracts/Bank/IBankApplication.cs
@@ -0,0 +1,17 @@
+﻿using _0_Framework.Application;
+
+namespace BankManagement.Application.Contracts.Bank
+{
+    public interface IBankApplication
+    {
+        OperationResult Create(CreateBank command);
+        OperationResult Edit(EditBank command);
+        OperationResult Remove(long id);
+        OperationResult Restore(long id);
+        OperationResult Activate(long id);
+        OperationResult Deactivate(long id);
+        EditBank GetDetails(long id);
+        List<BankViewModel> Search(BankSearchModel searchModel);
+        List<BankViewModel> GetBanks();
+    }
+}
---
+++ BankManagement.Domain/Bank/BankAgg/IBankRepository.cs
@@ -0,0 +1,12 @@
+﻿using _0_FrameWork.Domain;
+using BankManagement.Application.Contracts.Bank;
+
+namespace BankManagement.Domain.Bank.BankAgg
+{
+    public interface IBankRepository : IRepository<long, Banks>
+    {
+        EditBank GetDetails(long id);
+        List<BankViewModel> Search(BankSearchModel searchModel);
+        List<BankViewModel> GetBanks();
+    }
+}
