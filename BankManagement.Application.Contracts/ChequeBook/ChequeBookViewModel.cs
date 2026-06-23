7fd0844e40ba66b1ea07f7dc57fe771ff0eed0ef
Mostafa Abedi <mostafaabedi.developer@gamil.com>
2026-06-23 17:47:36 +0330

    ChequeBook Classes Implemented

---
+++ BankManagement.Application.Contracts/ChequeBook/ChequeBookViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace BankManagement.Application.Contracts.ChequeBook
+{
+    public class ChequeBookViewModel
+    {
+        public long Id { get; set; }
+        public int ChequeCount { get; set; }
+        public string FirstChequeCode { get; set; }
+        public string LastChequeCode { get; set; }
+        public string SerialNumber { get; set; }
+        public string ReceiveDate { get; set; }
+        public string Status { get; set; }
+        public string CompanyBankAccountTitle { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ BankManagement.Application.Contracts/ChequeBook/CreateChequeBook.cs
@@ -0,0 +1,12 @@
+﻿namespace BankManagement.Application.Contracts.ChequeBook
+{
+    public class CreateChequeBook
+    {
+        public int ChequeCount { get; set; }
+        public string FirstChequeCode { get; set; }
+        public string LastChequeCode { get; set; }
+        public string SerialNumber { get; set; }
+        public DateTime ReceiveDate { get; set; }
+        public long CompanyBankAccountId { get; set; }
+    }
+}
