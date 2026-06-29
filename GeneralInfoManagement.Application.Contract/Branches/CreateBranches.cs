933d3a1a066c46cef9d56a9b0cfdd6fd9a72b164
Mostafa Abedi <mostafaabedi.developer@gamil.com>
2026-06-29 13:06:50 +0330

    Fix Some Bugs From Pull

--- GeneralInfoManagement.Application.Contract/Branches/CreateBranches.cs
+++ GeneralInfoManagement.Application.Contract/Branches/CreateBranches.cs
@@ -9,22 +9,16 @@
         public string Email { get; set; }
         public string MobilePhone { get; set; }
         public string TelePhone { get; set; }
-
         public string Code { get; set; }
         public string Address { get; set; }
         public string PostCode { get; set; }
-
         public double Latitude { get; set; }
         public double Longitude { get; set; }
-
         public long CompanyId { get; set; }
         public long CityId { get; set; }
         public long ProvinceId { get; set; }
-
         public bool IsCodeAutomatic { get; set; } = true;
         public string? ManualCode { get; set; }
-
-        // ⭐ این دوتا رو اضافه کن
-        public bool IsMain { get; set; } = true;   // اصلی / فرعی
+        public bool IsMain { get; set; } = true;
     }
 }
