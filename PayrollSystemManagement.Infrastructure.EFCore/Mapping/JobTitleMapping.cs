e9dbbd473b95dd0cb6fb64e1e2fc5f34f1f9198c
Mostafa Abedi <mostafaabedi.developer@gamil.com>
2026-06-30 14:45:35 +0330

    Department And JobTitle Mapping And PayrollFakeDataContext Are Implemented

---
+++ PayrollSystemManagement.Infrastructure.EFCore/Mapping/JobTitleMapping.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PayrollSystemManagement.Domain.Payroll.JobTitleAgg;
+
+namespace PayrollSystemManagement.Infrastructure.EFCore.Mapping
+{
+    public class JobTitleMapping : IEntityTypeConfiguration<JobTitles>
+    {
+        public void Configure(EntityTypeBuilder<JobTitles> builder)
+        {
+            builder.ToTable("JobTitles");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Title).HasMaxLength(200).IsRequired();
+            builder.Property(x => x.Description).HasMaxLength(300).IsRequired(false);
+
+            builder.HasOne(x => x.Departments).WithMany(x => x.JobTitles).HasForeignKey(x => x.DepartmentId);
+
+
+
+        }
+    }
+}
