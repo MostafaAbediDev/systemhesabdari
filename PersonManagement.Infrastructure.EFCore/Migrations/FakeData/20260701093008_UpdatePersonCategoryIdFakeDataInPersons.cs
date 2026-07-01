53ac778d9aceea6a094d48056d2521b3226ce889
Mostafa Abedi <mostafaabedi.developer@gamil.com>
2026-07-01 13:04:29 +0330

    PersonCategoryId Added To Real And Fake Database And Implemented The FakeData

---
+++ PersonManagement.Infrastructure.EFCore/Migrations/FakeData/20260701093008_UpdatePersonCategoryIdFakeDataInPersons.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonManagement.Infrastructure.EFCore.Migrations.FakeData
+{
+    /// <inheritdoc />
+    public partial class UpdatePersonCategoryIdFakeDataInPersons : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+        UPDATE P
+        SET PersonCategoryId =
+        (
+            SELECT TOP (1) PC.Id
+            FROM PersonCategories PC
+            ORDER BY NEWID()
+        )
+        FROM Persons P
+        WHERE P.Id BETWEEN 1002 AND 1501;
+    ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ PersonManagement.Infrastructure.EFCore/Migrations/FakeData/20260701093208_UpdatePersonCategoryIdFakeDataInPersonsV2.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonManagement.Infrastructure.EFCore.Migrations.FakeData
+{
+    /// <inheritdoc />
+    public partial class UpdatePersonCategoryIdFakeDataInPersonsV2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+        UPDATE P
+SET PersonCategoryId = X.Id
+FROM Persons P
+CROSS APPLY
+(
+    SELECT TOP (1) Id
+    FROM PersonCategories
+    WHERE CHECKSUM(NEWID(), P.Id) IS NOT NULL
+    ORDER BY CHECKSUM(NEWID(), P.Id)
+) X
+WHERE P.Id BETWEEN 1002 AND 1501;
+    ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
