8572ac34f740d157d122da4147781502ab8ae76f
Mostafa Abedi <mostafaabedi.developer@gamil.com>
2026-07-01 13:33:36 +0330

    Department And JobTitle Fake Data Implemented

---
+++ PayrollSystemManagement.Infrastructure.EFCore/Migrations/FakeData/20260701100231_UpdateDepartmentIdInJobTitlesV2.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PayrollSystemManagement.Infrastructure.EFCore.Migrations.FakeData
+{
+    /// <inheritdoc />
+    public partial class UpdateDepartmentIdInJobTitlesV2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+        UPDATE JT
+        SET JT.DepartmentId =
+        (
+            SELECT TOP 1 D.Id
+            FROM Departments D
+            ORDER BY CHECKSUM(NEWID(), JT.Id)
+        )
+        FROM JobTitles JT;
+    ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
