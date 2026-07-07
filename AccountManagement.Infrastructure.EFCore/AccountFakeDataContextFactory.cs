f47bf1bfd77015c27c37af381a0cfc98680cb0b7
Mostafa Abedi <mostafaabedi.developer@gamil.com>
2026-07-07 12:14:00 +0330

    AccountDocumnet, Payroll And PayrollDetails FakeData Created

---
+++ AccountManagement.Infrastructure.EFCore/AccountFakeDataContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace AccountManagement.Infrastructure.EFCore
+{
+    public class AccountFakeDataContextFactory : IDesignTimeDbContextFactory<AccountFakeDataContext>
+    {
+        public AccountFakeDataContext CreateDbContext(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var options = new DbContextOptionsBuilder<AccountFakeDataContext>()
+                .UseSqlServer(config.GetConnectionString("TaadolFakeDb"))
+                .Options;
+
+            return new AccountFakeDataContext(options);
+        }
+    }
+}
