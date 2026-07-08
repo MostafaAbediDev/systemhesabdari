e8c1575b64d0c59629bdc02d30cccf346b7fd260
Mostafa Abedi <mostafaabedi.developer@gamil.com>
2026-07-08 12:38:57 +0330

    Financial Boostrapper Completed

--- FinancialManagement.Configuration/FinancialManagementBoostrapper.cs
+++ FinancialManagement.Configuration/FinancialManagementBoostrapper.cs
@@ -1,4 +1,11 @@
-﻿using FinancialManagement.Infrastructure.EFCore;
+﻿using FinancialManagement.Application;
+using FinancialManagement.Application.Contracts.Fund;
+using FinancialManagement.Application.Contracts.PettyCash;
+using FinancialManagement.Domain.FundAgg;
+using FinancialManagement.Domain.PettyCashAgg;
+using FinancialManagement.Domain.ReceiptsPaymentAgg;
+using FinancialManagement.Infrastructure.EFCore;
+using FinancialManagement.Infrastructure.EFCore.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -8,6 +15,15 @@ namespace FinancialManagement.Configuration
     {
         public static void Configure(IServiceCollection services, string connectionString)
         {
+            services.AddTransient<IFundRepository, FundRepository>();
+            services.AddTransient<IFundApplication, FundApplication>();
+
+            services.AddTransient<IPettyCashRepository, PettyCashRepository>();
+            services.AddTransient<IPettyCashApplication, PettyCashApplication>();
+
+            services.AddTransient<IReceiptsPaymentRepository, ReceiptsPaymentRepository>();
+            services.AddTransient<IReceiptsPaymentApplication, ReceiptsPaymentApplication>();
+
             services.AddDbContext<FinancialSystemContext>(x => x.UseSqlServer(connectionString));
             services.AddDbContext<FinancialFakeDataContext>(x => x.UseSqlServer(connectionString));
         }
